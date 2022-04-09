b126df18c4dcd7041ec6b6bbbe205135f7aa534e
Michał Damian Laskowski <01158164@pw.edu.pl>
2022-04-10 00:41:07 +0200

    Added untested methods for Basket and Warehouse classes

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -2,6 +2,36 @@ import java.util.Map;
 
 public class Basket {
     Map<Item, Integer> basket;
+    Warehouse warehouse;
 
+    public void resetBasket(){
+        if (!basket.isEmpty()){
+            basket.clear();
+        }
+    }
+
+    public void addItem(Item item){
+        if (warehouse.containItem(item) && warehouse.getQuantity(item)>0) {
+            if (basket.containsKey(item)){
+                Integer oldValue = basket.get(item);
+                basket.replace(item, oldValue + 1);
+            } else {
+                basket.put(item,1);
+            }
+        }
+    }
+
+    public void addMoreItems(Item item, Integer quantity){
+        for (int i=0; i < quantity; i++){
+            addItem(item);
+        }
+    }
+
+    public void buy(){
+        for (Item item : basket.keySet()){
+            warehouse.removeMoreItems(item, basket.get(item));
+        }
+        resetBasket();
+    }
 
 }
--- src/main/java/Warehouse.java
+++ src/main/java/Warehouse.java
@@ -14,17 +14,41 @@ public class Warehouse {
     }
 
     public void addItem(Item item){
-        if (items.containsKey(item)){
-            Integer oldValue = items.get(item);
+        if (containItem(item)){
+            Integer oldValue = getQuantity(item);
             items.replace(item, oldValue + 1);
         } else{
             items.put(item, 1);
         }
     }
 
-    public void addMoreItem(Item item, Integer quantity){
+    public void addMoreItems(Item item, Integer quantity){
         for (int i=0; i < quantity; i++){
             addItem(item);
         }
+    }
+
+    public void removeItem(Item item){
+        if (containItem(item) && getQuantity(item)>0){
+            Integer oldValue = getQuantity(item);
+            items.replace(item, oldValue - 1);
+        }
+    }
+
+    public void removeMoreItems(Item item, Integer quantity){
+        for (int i=0; i < quantity; i++){
+            removeItem(item);
+        }
+    }
+
+    public Integer getQuantity(Item item) {
+        if (containItem(item)) {
+            return items.get(item);
+        }
+        return -1;
+    }
+
+    public boolean containItem(Item item){
+        return items.containsKey(item);
     }
 }
