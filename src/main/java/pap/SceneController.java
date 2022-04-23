5012592ed8bcf60b2b0ccbba1bf8020669bb8260
Julita Wasilewska <01158318@pw.edu.pl>
2022-04-23 10:23:48 +0200

    reading from text fields

--- src/main/java/pap/SceneController.java
+++ src/main/java/pap/SceneController.java
@@ -1,18 +1,30 @@
 package pap;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private TextField login;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField productName;
+
 
     public void switchToMainShop(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("MainShop.fxml"));
@@ -35,5 +47,25 @@ public class SceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    public void switchToArticles(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Articles.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public Map tryLogging(MouseEvent event) throws IOException {
+        String log = login.getText();
+        String pass = password.getText();
+        Map<String, String> userData = new HashMap<String, String>();
+        userData.put(log, pass);
+        System.out.println(userData.keySet() + " " + userData.values());
+        return userData;
+    }
+    public String getArticleName(MouseEvent event) throws IOException {
+        String name = productName.getText().toLowerCase();
+        System.out.println(name);
+        return name;
     }
 }
