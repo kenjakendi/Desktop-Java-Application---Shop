768633cf839ee370a2d9f235258535ecfc19f578
Julita Wasilewska <01158318@pw.edu.pl>
2022-04-23 09:04:22 +0200

    Added opening window and basic switching between scenes

---
+++ src/main/java/pap/SceneController.java
@@ -0,0 +1,39 @@
+package pap;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToMainShop(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainShop.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void switchToPay(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Pay.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToLogIn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
