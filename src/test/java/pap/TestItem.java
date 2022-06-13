b296eedcf6236fb950c265b17e1d898adc9a9bbf
Michał Laskowski <01158164@pw.edu.pl>
2022-06-13 20:13:29 +0200

    Added Mockito to Maven dependency

--- src/test/java/pap/TestItem.java
+++ src/test/java/pap/TestItem.java
@@ -2,8 +2,24 @@ package pap;
 
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TestItem {
+
+    @Test
+    public void testMock(){
+        Item mockItem = Mockito.mock(Item.class);
+        Assert.assertNotNull(mockItem);
+        when(mockItem.getName()).thenReturn("mock");
+        String expectedName = "mock";
+        Assert.assertEquals(expectedName, mockItem.getName());
+    }
 
     @Test
     public void testId(){
